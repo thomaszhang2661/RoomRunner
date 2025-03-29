c2e0e42bfc89f4d7f6ac4d9e01f4f6b86a8e604a
zhangjian2661 <14090993+zhangjian2661@user.noreply.gitee.com>
2025-03-29 11:10:52 -0400

    before checkout to qiuchuan

---
+++ test/GameControllerTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import enginedriver.GameController;
+import enginedriver.GameWorld;
+
+public class GameControllerTest {
+
+}
