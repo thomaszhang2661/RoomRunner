e1238f050a14dda46e120d80cadb086e31531e54
Chuan-Qiu <jasonqidd@gmail.com>
2025-03-30 21:34:47 -0400

    enhance documentation for ItemSolutionValidator class and its validate method

--- src/enginedriver/problems/validator/ItemSolutionValidator.java
+++ src/enginedriver/problems/validator/ItemSolutionValidator.java
@@ -2,7 +2,20 @@ package enginedriver.problems.validator;
 
 import enginedriver.Item;
 
+/**
+ * Class for validating item solutions.
+ * This class implements the SolutionValidator interface
+ * and provides a method to validate item solutions.
+ */
 public class ItemSolutionValidator implements SolutionValidator<Item> {
+
+  /**
+   * Validates the item solution.
+   *
+   * @param problemSolution the correct item solution
+   * @param userInput the user's input item
+   * @return true if the solution is valid, false otherwise
+   */
   @Override
   public boolean validate(Item problemSolution, Item userInput) {
     return problemSolution.equals(userInput) && userInput.getRemainingUses() > 0;
